de13aaaad90364df29a24c0193fbada46d37a1ef
OrkanGoekcen <orkan.gok@gmail.com>
2026-03-24 13:27:59 +0100

    wallpaper_startup.sh now takes a random picture

--- hypr/scripts/wallpaper_startup.sh
+++ hypr/scripts/wallpaper_startup.sh
@@ -1,5 +1,13 @@
-#bin/bash
+#!/bin/bash
 
 # Wait until startup
 sleep 5
-hyprctl hyprpaper wallpaper 'DP-1, ~/.config/hypr/wallpapers/space.jpg, fill'
+
+# Wallpaper-Ordner
+WALLPAPER_DIR="$HOME/.config/hypr/wallpapers"
+
+# Zufälliges Wallpaper auswählen
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+
+# Setzen
+hyprctl hyprpaper wallpaper "DP-1,$WALLPAPER,fill"
