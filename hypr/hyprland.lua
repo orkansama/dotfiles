97d96274e739f3e9f30ad5c659da475a5aef6f18
OrkanGoekcen <orkan.gok@gmail.com>
2026-05-31 18:58:32 +0000

    style(hypr): enviroment_variables is now lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -10,15 +10,6 @@ require("input")
 require("window")
 require("autostart")
 
--------------------------------
----- ENVIRONMENT VARIABLES ----
--------------------------------
-
--- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
-
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
-
 -----------------------
 ----- PERMISSIONS -----
 -----------------------
