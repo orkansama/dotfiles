272d63631c3534be56adff84e436dfe66decde8b
OrkanGoekcen <orkan.gok@gmail.com>
2026-05-31 19:18:49 +0000

    append to 8f4139fcfc5b

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -52,17 +52,6 @@ hl.config({
 	},
 })
 
-----------------
-----  MISC  ----
-----------------
-
-hl.config({
-	misc = {
-		force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
-		disable_hyprland_logo = false, -- If true disables the random hyprland logo / anime girl background. :(
-	},
-})
-
 -- Example per-device config
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/ for more
 hl.device({
