a88bbfc502a644fd9d6dee98da9467b1e34705a4
OrkanGoekcen <orkan.gok@gmail.com>
2026-05-31 18:51:25 +0000

    style(hypr): autostart is now lua

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -8,20 +8,7 @@ hl.monitor({
 require("keybinds")
 require("input")
 require("window")
--------------------
----- AUTOSTART ----
--------------------
-
--- See https://wiki.hypr.land/Configuring/Basics/Autostart/
-
--- Autostart necessary processes (like notifications daemons, status bars, etc.)
--- Or execute your favorite apps at launch like this:
---
--- hl.on("hyprland.start", function ()
---   hl.exec_cmd(terminal)
---   hl.exec_cmd("nm-applet")
---   hl.exec_cmd("waybar & hyprpaper & firefox")
--- end)
+require("autostart")
 
 -------------------------------
 ---- ENVIRONMENT VARIABLES ----
