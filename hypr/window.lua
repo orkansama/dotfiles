55c924e673587fd40557467572528c684f0e9fcc
OrkanGoekcen <orkan.gok@gmail.com>
2026-06-01 21:24:06 +0000

    Added Prototype code

--- hypr/window.lua
+++ hypr/window.lua
@@ -1,11 +1,35 @@
 hl.window_rule({
-    name = "apply-opacity-everwhere",
+	name = "apply-opacity-everwhere",
 	match = { class = ".*" },
 	opacity = "0.95",
 })
 
 hl.window_rule({
-    name = "remove-firefox-opacity",
+	name = "remove-firefox-opacity",
 	match = { class = "firefox" },
-    opacity = "1"
+	opacity = "1",
+})
+
+hl.window_rule({
+	name = "terminal-WS1",
+	match = {
+		class = "alacritty",
+	},
+	workspace = "1",
+})
+
+hl.window_rule({
+	name = "firefox-WS2",
+	match = {
+		class = "firefox",
+	},
+	workspace = "2",
+})
+
+hl.window_rule({
+	name = "firefox-WS3",
+	match = {
+		class = "firefox",
+	},
+	workspace = "3",
 })
