b8e081bb6ee5877075054f441c228bb897186ced
OrkanGoekcen <orkan.gok@gmail.com>
2026-06-12 08:57:01 +0200

    refactor(workspace): workspaces now have names

--- hypr/workspacerules.lua
+++ hypr/workspacerules.lua
@@ -1,14 +1,17 @@
 hl.workspace_rule({
     workspace = "1",
+    default_name = "Code Editor",
     persistent = true,
 })
 
 hl.workspace_rule({
     workspace = "2",
+    default_name = "Browser",
     persistent = true,
 })
 
 hl.workspace_rule({
     workspace = "3",
+    default_name = "Data",
     persistent = true,
 })
