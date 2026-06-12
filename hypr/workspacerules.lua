d5e348e52b1ff43a18fa253238d838f6087293b9
OrkanGoekcen <orkan.gok@gmail.com>
2026-06-12 08:59:17 +0200

    refactor(workspac): default_name removed

--- hypr/workspacerules.lua
+++ hypr/workspacerules.lua
@@ -1,18 +1,15 @@
 hl.workspace_rule({
     workspace = "1",
-    default_name = "Code Editor",
     default = true,
     persistent = true,
 })
 
 hl.workspace_rule({
     workspace = "2",
-    default_name = "Browser",
     persistent = true,
 })
 
 hl.workspace_rule({
     workspace = "3",
-    default_name = "Data",
     persistent = true,
 })
