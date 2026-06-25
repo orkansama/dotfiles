e46cb76c853d3ac352b0dd070d03e315b4b944c6
OrkanGoekcen <orkan.gok@gmail.com>
2026-06-25 08:24:25 +0200

    feat: added settings.json to make wallaper script adjustable

--- hypr/scripts/wallpaper_startup.sh
+++ hypr/scripts/wallpaper_startup.sh
@@ -10,4 +10,16 @@ WALLPAPER_DIR="$HOME/.config/hypr/wallpapers"
 WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
 
 # Setzen
-hyprctl hyprpaper wallpaper "eDP-1,$WALLPAPER,fill"
+FirstMonitor="$( sudo cat ../settings.json | jq -r '.firstMonitor' )"
+SecondMonitor="$( sudo cat ../settings.json | jq -r '.secondMonitor' )"
+WallpaperScriptEnabled="$( sudo cat ../settings.json | jq -r '.wallpaperScriptEnabled' )"
+
+if [[ "$WallpaperScriptEnabled" ]]; then
+    if [[ "$FirstMonitor" != null  ]]; then
+        hyprctl hyprpaper wallpaper "$FirstMonitor,$WALLPAPER,fill"
+    fi
+
+    if [[ "$SecondMonitor" != null  ]]; then
+        hyprctl hyprpaper wallpaper "$SecondMonitor,$WALLPAPER,fill"
+    fi
+fi
